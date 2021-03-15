97bdaea18c6aa4f2880c970c4633d0e5d7f2ee5f
Clint Siadis <clint-siadis@hotmail.com>
2021-03-15 19:16:52 +0100

    Added company info modal

--- src/components/CompanyGrid/CompanyGrid.tsx
+++ src/components/CompanyGrid/CompanyGrid.tsx
@@ -1,5 +1,6 @@
 import { CSSProperties, useState, useEffect } from 'react';
-import { Card } from 'antd';
+import { Card, Modal, Button } from 'antd';
+import { CompanyInfoModal } from '../Modals/companyInfoModal';
 
 const gridStyle: CSSProperties = {
   width: '25%',
@@ -18,37 +19,53 @@ function CompanyGrid() {
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const [items, setItems] = useState([]);
+  const [visible, setVisible] = useState(false);
 
   useEffect(() => {
-      fetch(`${baseUrl}/companies`)
+    fetch(`${baseUrl}/companies`)
       .then(res => res.json())
       .then(
-          (result) => {
+        (result) => {
           setIsLoaded(true);
           setItems(result.response);
-          },
-          (error) => {
+        },
+        (error) => {
           setIsLoaded(true);
           setError(error);
-          }
+        }
       )
   }, [])
 
   if (error) {
-      return <div>Error: {error.message}</div>;
+    return <div>Error: {error.message}</div>;
   } else if (!isLoaded) {
-      return <Card title="Bedrijven" loading={true}></Card>;
+    return <Card title="Bedrijven" loading={true}></Card>;
   } else {
-      return (
-      <Card title="Bedrijven">
-        {items.map((company: any) => 
-          <a href={company.website} key={company.id}>
-            <Card.Grid style={gridStyle}>
-              <img src={`${baseUrl}${company.logo}`} style={imgStyle} alt={`${company.name} logo`}></img>
-            </Card.Grid>
-          </a>
-        )}
-      </Card>
+    return (
+      <>
+        <Modal
+          title="Modal 1000px width"
+          centered
+          visible={visible}
+          onOk={() => setVisible(false)}
+          onCancel={() => setVisible(false)}
+          width={1000}
+        >
+          <p>some contents...</p>
+          <p>some contents...</p>
+          <p>some contents...</p>
+        </Modal>
+
+        <Card title="Bedrijven">
+          {items.map((company: any) =>
+            <a onClick={() => setVisible(true)} key={company.id}>
+              <Card.Grid style={gridStyle}>
+                <img src={`${baseUrl}${company.logo}`} style={imgStyle} alt={`${company.name} logo`}></img>
+              </Card.Grid>
+            </a>
+          )}
+        </Card>
+      </>
     );
   }
 }
