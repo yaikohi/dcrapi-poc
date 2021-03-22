6533855c160858cba58227bd65836da7a44f7665
Michael de Rooij <michael.derooij@student.hu.nl>
2021-03-22 14:23:36 +0100

    Create search bar element

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
 import './App.css';
 import { CompanyGrid } from './components/CompanyGrid/CompanyGrid';
+import { Input } from 'antd';
+import { Layout } from 'antd';
+
+const { Search } = Input;
+const { Header, Content  } = Layout;
+
+const gapSize = 16;
 
 function App() {
-  return <CompanyGrid />
+  return (
+    <Layout className="site-layout">
+        <Header className="site-layout-background center" style={{ padding: `${gapSize}px` }}>
+          <Search className="search-bar" placeholder="Bedrijfsnaam" enterButton />
+        </Header>
+        <Content style={{ margin: `${gapSize}px` }}>
+          <CompanyGrid />
+        </Content>
+    </Layout>
+      
+  )
 }
 
 export default App;
