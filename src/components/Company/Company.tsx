7a33ef55d5a9e7015307c365bfc4f25504b7eb29
Michael de Rooij <michael.derooij@student.hu.nl>
2021-04-01 12:49:33 +0200

    Place columns in cards and make them responsive

--- src/components/Company/Company.tsx
+++ src/components/Company/Company.tsx
@@ -34,27 +34,31 @@ export const Company: FC<CompanyProps> = (props) => {
         </Breadcrumb>
         <Card className="main-card">
           <Row wrap={true} className="modal-row">
-            <Col span={6} className="modal-column">
+            <Col xs={24} md={7} className="modal-column">
               <Row justify="center" align="middle" className="logo-row">
                 <CompanyLogo logoName={props.company.logo} />
               </Row>
             </Col>
-            <Col span={18} className="company-details">
+            <Col xs={24} md={17} className="company-details">
               <CompanyDetails companyName={props.company.name} companyTagline={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."} />
             </Col>
           </Row>
           
           <Row className="modal-row">
-            <Card title="Over dit bedrijf">
+            <Card title="Over dit bedrijf" className="card">
               <CompanyDescr companyDescr={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."} />
             </Card>
           </Row>
-          <Row className="modal-row">
-            <Col span={12} className="modal-column">
-              <CompanyEvaluaties />
+          <Row className="modal-row" gutter={[20, 20]} wrap={true}>
+            <Col xs={24} md={12} className="modal-column">
+              <Card title="Evaluaties" className="card">
+                <CompanyEvaluaties />
+              </Card>
             </Col>
-            <Col span={12} className="modal-column">
-              <CompanyContactInfo website={props.company.website} contacts={props.company.contacts} />
+            <Col xs={24} md={12} className="modal-column">
+              <Card title="Contactgegevens" className="card">
+                <CompanyContactInfo website={props.company.website} contacts={props.company.contacts} />
+              </Card>
             </Col>
           </Row>
         </Card>
