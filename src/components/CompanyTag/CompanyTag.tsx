90f59b7e14862380283953fb769917719dc1bce9
Erik Beem <erik.beem@hotmail.com>
2021-04-20 10:20:43 +0200

    Created CompanyTag.tsx

---
+++ src/components/CompanyTag/CompanyTag.tsx
@@ -0,0 +1,14 @@
+import { Tag } from 'antd';
+import { Typography } from 'antd';
+
+const { Text } = Typography;
+
+export const CompanyTag: React.FC = () => {
+  return (
+    <Tag>
+      <Text>
+        IT
+      </Text>
+    </Tag>
+  )
+}
