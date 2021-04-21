6f4163a84dd73dc056453c70f0b314cc58226d12
Clint Siadis <clint-siadis@hotmail.com>
2021-04-21 08:37:06 +0200

    Add statistic component to show scores

--- src/components/CompanyEvaluaties/CompanyEvaluaties.tsx
+++ src/components/CompanyEvaluaties/CompanyEvaluaties.tsx
@@ -1,6 +1,14 @@
-import { Typography } from 'antd';
+import { Row, Col, Typography, Statistic } from 'antd';
 const { Text } = Typography;
 
 export const CompanyEvaluaties: React.FC = () => {
-  return <Text>Evaluaties zijn op dit moment nog niet beschikbaar.</Text>;
+  return (
+    <>
+      <Row gutter={16}>
+        <Col span={24}>
+          <Statistic title="Organisatie" value={8} precision={1} />
+        </Col>
+      </Row>
+    </>
+  );
 }
