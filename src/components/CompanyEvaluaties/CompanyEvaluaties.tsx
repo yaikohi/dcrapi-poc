d446030ebcb89191e54b2fab8ff466c00a87e570
Clint Siadis <clint-siadis@hotmail.com>
2021-04-21 11:33:29 +0200

    Add multiple scores in evaluation section

--- src/components/CompanyEvaluaties/CompanyEvaluaties.tsx
+++ src/components/CompanyEvaluaties/CompanyEvaluaties.tsx
@@ -5,9 +5,20 @@ export const CompanyEvaluaties: React.FC = () => {
   return (
     <>
       <Row gutter={16}>
-        <Col span={24}>
-          <Statistic title="Organisatie" value={8} precision={1} />
+
+        <Col span={6}>
+          <Statistic title="Organization of Cooperation" value={7.8} />
         </Col>
+        <Col span={6}>
+          <Statistic title="Quality of education" value={8.7} />
+        </Col>
+        <Col span={6}>
+          <Statistic title="Adequate knowledge to students" value={8.1} />
+        </Col>
+        <Col span={6}>
+          <Statistic title="Cooperation last year" value={6.3} />
+        </Col>
+
       </Row>
     </>
   );
