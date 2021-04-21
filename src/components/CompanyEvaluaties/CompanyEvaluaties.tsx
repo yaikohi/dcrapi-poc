bc7649c60bc4f0d8620900b5141b293babf72b66
Clint Siadis <clint-siadis@hotmail.com>
2021-04-21 12:20:22 +0200

    Add custom styling for score items

--- src/components/CompanyEvaluaties/CompanyEvaluaties.tsx
+++ src/components/CompanyEvaluaties/CompanyEvaluaties.tsx
@@ -1,22 +1,24 @@
 import { Row, Col, Typography, Statistic } from 'antd';
+import './CompanyEvaluaties.css';
+
 const { Text } = Typography;
 
 export const CompanyEvaluaties: React.FC = () => {
   return (
     <>
-      <Row gutter={8}>
+      <Row gutter={8} >
         <Col span={18}>
-          <Row gutter={8}>
-            <Col span={12}>
-              <Statistic title="Organization of Cooperation" value={7.8} />
+          <Row gutter={8} className="evaluaties-scores-section">
+            <Col span={12} className="evaluation-item">
+              <Statistic title="Organization of Cooperation" value={7.8}/>
             </Col>
-            <Col span={12}>
+            <Col span={12} className="evaluation-item">
               <Statistic title="Adequate knowledge to students" value={8.1} />
             </Col>
-            <Col span={12}>
+            <Col span={12} className="evaluation-item">
               <Statistic title="Organization of Cooperation" value={7.8} />
             </Col>
-            <Col span={12}>
+            <Col span={12} className="evaluation-item">
               <Statistic title="Quality of education" value={8.7} />
             </Col>
           </Row>
