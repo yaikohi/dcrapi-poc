ce86056df8aa92f0369f8f0d8f87b50cf1054ff0
Clint Siadis <clint-siadis@hotmail.com>
2021-04-23 01:11:57 +0200

    Add badge to average score card

--- src/components/CompanyEvaluaties/CompanyEvaluaties.tsx
+++ src/components/CompanyEvaluaties/CompanyEvaluaties.tsx
@@ -1,4 +1,4 @@
-import { Row, Col, Typography, Statistic, Card, Divider, Rate } from 'antd';
+import { Row, Col, Typography, Statistic, Card, Divider, Rate, Badge } from 'antd';
 import { FrownOutlined, MehOutlined, SmileOutlined } from '@ant-design/icons';
 import ColumnGroup from 'antd/lib/table/ColumnGroup';
 import './CompanyEvaluaties.css';
@@ -34,19 +34,21 @@ export const CompanyEvaluaties: React.FC = () => {
           </Row>
         </Col>
         <Col md={6}>
-          <Card
-            hoverable
-            className="center average-score-item"
-            style={{
-              backgroundColor: "#1890ff", color: "#fff"
-            }}
-          >
-            <Text className="evaluaties-score-number">7.8</Text>
-            <Divider type="horizontal" className="bg-white" />
-            <Title level={3} style={{ color: "#fff" }}>AVERAGE SCORE</Title>
-            <Rate defaultValue={3} character={({ index }) => customIcons[index + 1]} />
-            {/* <Text style={{ color: "#fff" }}>AVERAGE SCORE</Text> */}
-          </Card>
+          <Badge.Ribbon style={{ backgroundColor: "#e6302b" }} text="Thanks for the cooperation">
+            <Card
+              hoverable
+              className="center average-score-item"
+              style={{
+                backgroundColor: "#1890ff", color: "#fff"
+              }}
+            >
+              <Text className="evaluaties-score-number">7.8</Text>
+              <Divider type="horizontal" className="bg-white" />
+              <Title level={3} style={{ color: "#fff" }}>AVERAGE SCORE</Title>
+              <Rate defaultValue={3} character={({ index }) => customIcons[index + 1]} />
+              {/* <Text style={{ color: "#fff" }}>AVERAGE SCORE</Text> */}
+            </Card>
+          </Badge.Ribbon>
         </Col>
       </Row>
     </>
