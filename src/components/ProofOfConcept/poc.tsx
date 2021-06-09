937332680659b34918c549291839c5279b68b9bf
Erik Beem <erik.beem@hotmail.com>
2021-06-09 11:10:27 +0200

    Added local colorDb for poc

--- src/components/ProofOfConcept/poc.tsx
+++ src/components/ProofOfConcept/poc.tsx
@@ -1,59 +1,62 @@
 import { Company as CompanyType } from "../../interfaces/Company";
-import { CompanyDescr } from "../CompanyDescr/CompanyDescr";
 import { CompanyContactInfo } from "../CompanyContactInfo/CompanyContactInfo";
-import axios from "axios";
 
-import { useEffect, useState } from "react";
+import CSS from "csstype";
 import { Card, Row, Col } from "antd";
 
-type DcrApiResp = [String, String, String];
+const colorDb = {
+  "Fynch": ["10998c"],
+  "Centric": ["171518", "5ca62f"],
+  "Inergy": ["000000", "918f94", "b60106"],
+  "Quintor": ["cb95a0", "fef1f3", "a13c4b"],
+  "Musketeers.group": ["5ec301", "838681"],
+  "Terberg Group": ["1e1e1c", "c99509"],
+  "Pegasystems": ["1f2555", "04a5a8"],
+  "Info Support International Group B.V.": ["5391bf", "a8c9e2"],
+  "Shopping Minds": ["fcfbfc", "2b1f48", "9d8299"],
+  "Vodafone Ziggo": ["000000", "ed5802", "d0cdcf"],
+  "Capgemini Nederland BV": ["0070ad", "12abdb"],
+  "Alstom Transport BV": ["174385", "f5f7fa", "c93d48"],
+  "AFAS Software": ["1564a4", "c80e42"],
+  "Sogeti Nederland B.V.": ["0071ae", "ff4019"],
+  "Rabobank": ["070494", "ec781e"],
+  "Hydrologic": ["70ab37", "4982cf"],
+  "Proact": [, "f5002e", "f4012e"],
+  "Axians": ["000000", "0072bc", "a60771"],
+  "Yellax": ["fedf00", "070501", "936b17"],
+  "CIM Solutions": ["6f9c42", "c5da99"],
+  "Finaps B.V.": ["191970", "ffaa00"],
+};
 
-interface Props {
+interface PocProps {
   company: CompanyType;
 }
 
-export const Poc = ({ company, ...props }: Props) => {
-  const [colors, setColors] = useState([]);
-  const [loaded, setLoaded] = useState(false);
+export const Poc = ({company}: PocProps) => {
+  // CSS backgroundColor variables
+  const companyColorsBase = colorDb[company.name][1];
+  const companyColorsHue: String = "ff";
 
+  const companyColors = companyColorsBase + companyColorsHue;
 
-  useEffect(() => {
-    const fetchColors = async () => {
-      try {
-        const response = await axios.get<DcrApiResp>(
-          `http://dcr-api000.herokuapp.com/api/piodash-colors/${company.id}`
-        );
-        const data = response.data
-        setColors(data);
-        console.log(colors)
-        setLoaded(true)
-      } catch (error) {
-        if (error) {
-          console.log(error.message);
-          return error.message;
-        }
-      }
-    };
-    fetchColors();
-  }, colors);
+  // Style variables
+  const aboutStyle: CSS.Properties = {
+    backgroundColor: `#${companyColors}`,
+  };
 
+  const emptyStyle: CSS.Properties = {
+    backgroundColor: 'white'
+  }
   return (
     <>
-      {loaded ? (
         <Card>
-          <Card className="main-card">
+          <Card className="main-card" style={emptyStyle} >
             <Row className="modal-row" gutter={[20, 20]} wrap={true}>
               <Col xs={24} md={16} className="modal-column">
-                <Card title="Over dit bedrijf" className="card card__contacts">
-                  <CompanyDescr
-                    companyDescr={
-                      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-                    }
-                  />
-                </Card>
+                <Card.Meta title="Over dit bedrijf" style={aboutStyle} description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." />
               </Col>
               <Col xs={24} md={8} className="modal-column">
-                <Card title="Contactgegevens" className="card card__contacts">
+                <Card style={aboutStyle}  title="Contactgegevens">
                   <CompanyContactInfo
                     website={company.website}
                     contacts={company.contacts}
@@ -63,9 +66,6 @@ export const Poc = ({ company, ...props }: Props) => {
             </Row>
           </Card>
         </Card>
-      ) : (
-        <h2> Loading ... </h2>
-      )}
     </>
   );
 };
