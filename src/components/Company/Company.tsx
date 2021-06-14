23507a45de4486c401b6929853f8b71450577590
Erik Beem <erik.beem@hotmail.com>
2021-06-14 10:00:42 +0200

    Integrated POC to Company.tsx

--- src/components/Company/Company.tsx
+++ src/components/Company/Company.tsx
@@ -1,10 +1,10 @@
-import { Company as CompanyType } from '../../interfaces/Company'
-import { CompanyLogo } from '../CompanyLogo/CompanyLogo';
-import { CompanyEvaluaties } from '../CompanyEvaluaties/CompanyEvaluaties';
-import { CompanyDescr } from '../CompanyDescr/CompanyDescr';
-import { CompanyContactInfo } from '../CompanyContactInfo/CompanyContactInfo';
-import { CompanyDetails } from '../CompanyDetails/CompanyDetails';
-import './Company.css';
+import { Company as CompanyType } from "../../interfaces/Company";
+import { CompanyLogo } from "../CompanyLogo/CompanyLogo";
+import { CompanyEvaluaties } from "../CompanyEvaluaties/CompanyEvaluaties";
+import { CompanyDescr } from "../CompanyDescr/CompanyDescr";
+import { CompanyContactInfo } from "../CompanyContactInfo/CompanyContactInfo";
+import { CompanyDetails } from "../CompanyDetails/CompanyDetails";
+import "./Company.css";
 import { CompanyTag } from "../CompanyTag/CompanyTag";
 
 import { FC } from "react";
@@ -12,11 +12,52 @@ import { Card, Row, Col } from "antd";
 import { Breadcrumb } from "antd";
 import { Link } from "react-router-dom";
 import { HomeOutlined } from "@ant-design/icons";
+import CSS from "csstype";
+
 interface CompanyProps {
-  company: CompanyType
+  company: CompanyType;
 }
 
-export const Company: FC<CompanyProps> = (props) => {
+// TODO possibly refactor html element classnames
+// TODO refactor/select color values
+const colorDb = {
+  Fynch: ["10998c"],
+  Centric: ["5ca62f"],
+  Inergy: ["b60106"],
+  Quintor: ["a13c4b"],
+  "Musketeers.group": ["5ec301"],
+  "Terberg Group": ["c99509"],
+  Pegasystems: ["1f2555"],
+  "Info Support International Group B.V.": ["a8c9e2"],
+  "Shopping Minds": ["9d8299"],
+  "Vodafone Ziggo": ["d0cdcf"],
+  "Capgemini Nederland BV": ["12abdb"],
+  "Alstom Transport BV": ["174385"],
+  "AFAS Software": ["1564a4", "c80e42"],
+  "Sogeti Nederland B.V.": ["0071ae", "ff4019"],
+  Rabobank: ["ec781e"],
+  Hydrologic: ["4982cf"],
+  Proact: ["f5002e", "f4012e"],
+  Axians: ["0072bc", "a60771"],
+  Yellax: ["fedf00", "070501", "936b17"],
+  "CIM Solutions": ["6f9c42", "c5da99"],
+  "Finaps B.V.": ["191970", "ffaa00"],
+};
+
+export const Company: FC<CompanyProps> = ({ company }: CompanyProps) => {
+  // CSS backgroundColor variables
+  const companyColorsBase = colorDb[company.name][0];
+  const companyColorsHue: String = "35";
+
+  const companyColors = companyColorsBase + companyColorsHue;
+
+  // Style variables
+  const aboutStyle: CSS.Properties = {
+    backgroundColor: `#${companyColors}`,
+    borderRadius: "6px",
+    padding: "40px",
+    color: "#ffffff",
+  };
 
   return (
     <Card>
@@ -28,20 +69,26 @@ export const Company: FC<CompanyProps> = (props) => {
         </Breadcrumb.Item>
         <Breadcrumb.Item>
           <Link to="#">
-            <span>{props.company.name}</span>
+            <span>{company.name}</span>
           </Link>
         </Breadcrumb.Item>
       </Breadcrumb>
-      <Card className="main-card">
+
+      <Card className="main-card" style={aboutStyle}>
         <Row wrap={true} className="modal-row">
           <Col xs={24} md={7} className="modal-column">
             <Row justify="center" align="middle" className="logo-row">
-              <CompanyLogo logoName={props.company.logo} />
+              <CompanyLogo logoName={company.logo} />
             </Row>
           </Col>
           <Col xs={24} md={17} className="company-details">
-            <CompanyDetails companyName={props.company.name} companyTagline={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."} />
-            {props.company.sectors.map((sector) => (
+            <CompanyDetails
+              companyName={company.name}
+              companyTagline={
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+              }
+            />
+            {company.sectors.map((sector) => (
               <CompanyTag text={sector} />
             ))}
           </Col>
@@ -50,12 +97,19 @@ export const Company: FC<CompanyProps> = (props) => {
         <Row className="modal-row" gutter={[20, 20]} wrap={true}>
           <Col xs={24} md={16} className="modal-column">
             <Card title="Over dit bedrijf" className="card card__contacts">
-              <CompanyDescr companyDescr={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."} />
+              <CompanyDescr
+                companyDescr={
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                }
+              />
             </Card>
           </Col>
           <Col xs={24} md={8} className="modal-column">
             <Card title="Contactgegevens" className="card card__contacts">
-              <CompanyContactInfo website={props.company.website} contacts={props.company.contacts} />
+              <CompanyContactInfo
+                website={company.website}
+                contacts={company.contacts}
+              />
             </Card>
           </Col>
         </Row>
@@ -63,11 +117,11 @@ export const Company: FC<CompanyProps> = (props) => {
         <Row className="modal-row">
           <Col xs={24} md={24} className="modal-column">
             <Card title="Evaluaties" className="card">
-              <CompanyEvaluaties  company={props.company.id}/>
+              <CompanyEvaluaties company={company.id} />
             </Card>
           </Col>
         </Row>
       </Card>
     </Card>
   );
-}
+};
--- src/components/ProofOfConcept/poc.tsx
+++ src/components/ProofOfConcept/poc.tsx
@@ -6,23 +6,23 @@ import { Card, Row, Col } from "antd";
 
 const colorDb = {
   "Fynch": ["10998c"],
-  "Centric": ["171518", "5ca62f"],
-  "Inergy": ["000000", "918f94", "b60106"],
-  "Quintor": ["cb95a0", "fef1f3", "a13c4b"],
-  "Musketeers.group": ["5ec301", "838681"],
-  "Terberg Group": ["1e1e1c", "c99509"],
-  "Pegasystems": ["1f2555", "04a5a8"],
-  "Info Support International Group B.V.": ["5391bf", "a8c9e2"],
-  "Shopping Minds": ["fcfbfc", "2b1f48", "9d8299"],
-  "Vodafone Ziggo": ["000000", "ed5802", "d0cdcf"],
-  "Capgemini Nederland BV": ["0070ad", "12abdb"],
-  "Alstom Transport BV": ["174385", "f5f7fa", "c93d48"],
+  "Centric": ["5ca62f"],
+  "Inergy": ["b60106"],
+  "Quintor": ["a13c4b"],
+  "Musketeers.group": ["5ec301"],
+  "Terberg Group": ["c99509"],
+  "Pegasystems": ["1f2555"],
+  "Info Support International Group B.V.": ["a8c9e2"],
+  "Shopping Minds": ["9d8299"],
+  "Vodafone Ziggo": ["d0cdcf"],
+  "Capgemini Nederland BV": ["12abdb"],
+  "Alstom Transport BV": ["174385"],
   "AFAS Software": ["1564a4", "c80e42"],
   "Sogeti Nederland B.V.": ["0071ae", "ff4019"],
-  "Rabobank": ["070494", "ec781e"],
-  "Hydrologic": ["70ab37", "4982cf"],
-  "Proact": [, "f5002e", "f4012e"],
-  "Axians": ["000000", "0072bc", "a60771"],
+  "Rabobank": ["ec781e"],
+  "Hydrologic": ["4982cf"],
+  "Proact": ["f5002e", "f4012e"],
+  "Axians": ["0072bc", "a60771"],
   "Yellax": ["fedf00", "070501", "936b17"],
   "CIM Solutions": ["6f9c42", "c5da99"],
   "Finaps B.V.": ["191970", "ffaa00"],
@@ -34,7 +34,7 @@ interface PocProps {
 
 export const Poc = ({company}: PocProps) => {
   // CSS backgroundColor variables
-  const companyColorsBase = colorDb[company.name][1];
+  const companyColorsBase = colorDb[company.name][0];
   const companyColorsHue: String = "35";
 
   const companyColors = companyColorsBase + companyColorsHue;
@@ -44,12 +44,13 @@ export const Poc = ({company}: PocProps) => {
     backgroundColor: `#${companyColors}`,
     borderRadius: "6px",
     padding: "40px",
-    color: "ffffff",
+    color: "#ffffff",
     
   };
 
   const emptyStyle: CSS.Properties = {
-    backgroundColor: 'white'
+    backgroundColor: 'white',
+    color: "white",
   }
   return (
     <>
