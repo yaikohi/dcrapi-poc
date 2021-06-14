202d6a56229e148ce9da744a01325c91c556056d
Erik Beem <erik.beem@hotmail.com>
2021-06-14 10:01:04 +0200

    Added optional CSS style prop

--- src/components/CompanyDetails/CompanyDetails.tsx
+++ src/components/CompanyDetails/CompanyDetails.tsx
@@ -1,9 +1,12 @@
 import { Typography } from 'antd';
+import CSS from "csstype";
+
 const { Paragraph, Title } = Typography;
 
 interface CompanyDetailsProps {
   companyName: string;
   companyTagline: string;
+  style?: CSS.Properties;  // allows inline CSS styling inside parent components
 }
 
 export const CompanyDetails: React.FC<CompanyDetailsProps> = ({ companyName, companyTagline }: CompanyDetailsProps) => {
