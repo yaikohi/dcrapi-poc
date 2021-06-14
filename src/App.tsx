50b7d451a7c3078e15276a988bf1fc792e9ea9a1
Erik Beem <erik.beem@hotmail.com>
2021-06-14 10:00:53 +0200

    Integrated POC -> Company.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,66 @@
-import { useState } from 'react';
-import { useRequest } from '../src/helpers/axios';
-import './App.css';
-import { CompanyGrid } from './components/CompanyGrid/CompanyGrid';
-import { SiteHeader } from './components/SiteHeader/SiteHeader';
+import { useState } from "react";
+import { useRequest } from "../src/helpers/axios";
+import "./App.css";
+import { CompanyGrid } from "./components/CompanyGrid/CompanyGrid";
+import { SiteHeader } from "./components/SiteHeader/SiteHeader";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { Company } from './components/Company/Company';
-import { Layout } from 'antd';
+import { Company } from "./components/Company/Company";
+import { Layout } from "antd";
 
-import { Poc } from '../src/components/ProofOfConcept/poc';
 const { Content } = Layout;
 
 const gapSize = 16;
 const baseUrl: string = process.env.REACT_APP_API_URL;
 
 function App() {
-    const [searchInput, setSearchInput] = useState("");
-    const [companiesFetchError, setCompaniesFetchError] = useState(null);
-    let companiesFetched = false;
+  const [searchInput, setSearchInput] = useState("");
+  const [companiesFetchError, setCompaniesFetchError] = useState(null);   // ! setCompaniesFetchError is unused
+  let companiesFetched = false;
 
-    const data = useRequest(`${baseUrl}/companies`);
+  const data = useRequest(`${baseUrl}/companies`);
 
-    if (!data) {
-        return null;
-    }
+  if (!data) {
+    return null;
+  }
 
-    const companyRoutes = data.response.map((company) => {
-        companiesFetched = true;
-        return (
-            <Route exact path={`/company/${company.id}`}>
-                <Poc company={company} />
-            </Route>
-        )
-    })
-
+  const companyRoutes = data.response.map((company) => {
+    companiesFetched = true;
     return (
-        <Layout className="site-layout">
-            <Router>
-                <SiteHeader setSearchInput={setSearchInput} />
-                <Content className="center" style={{ margin: `${gapSize}px 0px 0px 0px`, padding: `0px ${gapSize}px 0px ${gapSize}px`, width: "100%" }}>
-                    <Content style={{ maxWidth: "1500px" }}>
-                        <Switch>
-                            <Route exact path="/">
-                                <CompanyGrid
-                                    companies={data.response}
-                                    searchInput={searchInput}
-                                    companiesFetched={companiesFetched}
-                                    companiesFetchError={companiesFetchError}
-                                />
-                            </Route>
-                            {companyRoutes}
-                        </Switch>
-                    </Content>
-                </Content>
-            </Router>
-        </Layout>
-    )
+      <Route exact path={`/company/${company.id}`}>
+        <Company company={company} />
+      </Route>
+    );
+  });
+
+  return (
+    <Layout className="site-layout">
+      <Router>
+        <SiteHeader setSearchInput={setSearchInput} />
+        <Content
+          className="center"
+          style={{
+            margin: `${gapSize}px 0px 0px 0px`,
+            padding: `0px ${gapSize}px 0px ${gapSize}px`,
+            width: "100%",
+          }}
+        >
+          <Content style={{ maxWidth: "1500px" }}>
+            <Switch>
+              <Route exact path="/">
+                <CompanyGrid
+                  companies={data.response}
+                  searchInput={searchInput}
+                  companiesFetched={companiesFetched}
+                  companiesFetchError={companiesFetchError}
+                />
+              </Route>
+              {companyRoutes}
+            </Switch>
+          </Content>
+        </Content>
+      </Router>
+    </Layout>
+  );
 }
 
 export default App;
